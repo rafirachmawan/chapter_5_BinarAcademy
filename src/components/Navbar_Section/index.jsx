4bef23cc26e0b88d508d15162140eca3c067040a
Rafi Rachmawan <rafirachmawan1987@gmail.com>
2023-11-30 00:40:24 +0700

    add css on navbar

--- src/components/Navbar_Section/index.jsx
+++ src/components/Navbar_Section/index.jsx
@@ -6,12 +6,15 @@ const Navbar_section = () => {
   return (
     <div className="navbar-container">
       <div className="text-container">
-        <p>Sewa & Rental Mobil Terbaik di kawasan (Lokasimu)</p>
-        <p>
+        <p className="sewa">
+          Sewa & Rental Mobil Terbaik di kawasan (Lokasimu)
+        </p>
+        <p className="selamat">
           Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas
           terbaik dengan harga terjangkau. Selalu siap melayani kebutuhanmu
           untuk sewa mobil selama 24 jam.
         </p>
+        <button>Mulai Sewa Mobil</button>
       </div>
       <div className="logo-container">
         <img src={logoimg} alt="" />
