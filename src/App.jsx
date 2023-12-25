fb236d5ccabe47279039e05abd7f5fd055d405ce
Rafi Rachmawan <rafirachmawan1987@gmail.com>
2023-12-25 16:02:05 +0700

    add DetailCar on Pages and API

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import LandingPages from "./pages/LandingPages";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DetailCar from "./pages/DetailCar";
 
 function App() {
   return (
-    <>
-      <LandingPages />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPages />} />
+        <Route path="/detail/:id" element={<DetailCar />} />
+        // <Route path="" element={<LandingPages />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/DetailCar/index.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { Container, Card } from "react-bootstrap";
+import "./style.css";
+
+const DetailCar = () => {
+  const param = useParams();
+  const [carDetail, setCarDetail] = useState({});
+
+  useEffect(() => {
+    handleCarDetail();
+  }, []);
+
+  const handleCarDetail = () => {
+    axios
+      .get(`https://api-car-rental.binaracademy.org/customer/car/${param.id}`)
+
+      .then((res) => {
+        console.log(res.data);
+        setCarDetail(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  let includeList = [
+    { include: "Apa saja yang termasuk dalam paket misal durasi max 12 jam" },
+    { include: "Sudah termasuk bensin selama 12 jam" },
+    { include: "Sudah termasuk Tiket Wisata" },
+    { include: "Sudah termasuk pajak" },
+  ];
+  let exculdeList = [
+    { exclude: "Tidak termasuk biaya makan sopir Rp 75.000/hari" },
+    {
+      exclude:
+        "Jika overtime lebih dari 12 jam akan ada tambahan biaya Rp 20.000/jam",
+    },
+    { exclude: "Tidak termasuk akomodasi penginapan" },
+  ];
+  let rulesList = [
+    { rules: "Tidak termasuk biaya makan sopir Rp 75.000/hari" },
+    {
+      rules:
+        "Jika overtime lebih dari 12 jam akan ada tambahan biaya Rp 20.000/jam",
+    },
+    { rules: "Tidak termasuk akomodasi penginapan" },
+    { rules: "Tidak termasuk biaya makan sopir Rp 75.000/hari" },
+    {
+      rules:
+        "Jika overtime lebih dari 12 jam akan ada tambahan biaya Rp 20.000/jam",
+    },
+    { rules: "Tidak termasuk akomodasi penginapan" },
+    { rules: "Tidak termasuk biaya makan sopir Rp 75.000/hari" },
+    {
+      rules:
+        "Jika overtime lebih dari 12 jam akan ada tambahan biaya Rp 20.000/jam",
+    },
+    { rules: "Tidak termasuk akomodasi penginapan" },
+  ];
+  return (
+    <div>
+      <Navbar />
+      <div className="car-info">
+        <div className="rent-rules">
+          <Card />
+          <h4>Tentang Paket</h4>
+          <h6>Include</h6>
+          {include.map((inc) => {
+            return <p> .{inc.include}</p>;
+          })}
+          <h6>exclude</h6>
+          {exclude.map((exc) => {
+            return <p> .{exc.exclude}</p>;
+          })}
+          <h6>Refund, Resclude, Overtime</h6>
+          {rulesList.map((rul) => {
+            return <p>. {rul.rules}</p>;
+          })}
+          <Card />
+        </div>
+        <div className="car-info-3">
+          <Card />
+          <img src={carDetail.image} />
+          <h5>{carDetail.name}</h5>
+          <p>6-8 orang</p>
+          <h6>Total:{carDetail.price}</h6>
+          <Card />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default DetailCar;
