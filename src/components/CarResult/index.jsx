e2a0d377e43111aeb5a084c5a66b7066e0e7d47f
Rafi Rachmawan <rafirachmawan1987@gmail.com>
2023-12-29 01:33:29 +0700

    add carResult and page

---
+++ src/components/CarResult/index.jsx
@@ -0,0 +1,131 @@
+import React from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { Container, Card, Row, Col } from "react-bootstrap";
+import "./style.css";
+import { useEffect, useState } from "react";
+
+const CarResult = () => {
+  const [car, setCar] = useState([]);
+  const [name, setName] = useState([]);
+  const [type, setType] = useState([]);
+  const [price, setPrice] = useState([]);
+  const [status, setStatus] = useState([]);
+  const [isFilter, setIsfilter] = useState([]);
+
+  useEffect(() => {
+    handleGetCar();
+  }, []);
+
+  const handleGetCar = (isName, isType, isPrice, isStatus) => {
+    axios
+      .get(
+        `https://api-car-rental.binaracademy.org/customer/v2/car?name=${isName}&category=${isType}&minPrice&maxPrice=${isPrice}&isRented=${isStatus}`
+      )
+      .then((res) => {
+        console.log(res);
+        setCar(res.data.cars);
+      })
+      .catch((err) => console.log(err));
+  };
+  const handleChangeName = (e) => {
+    setName(e.target.value);
+  };
+  const handleSubmit = () => {
+    console.log(name);
+    handleGetCar(name, type, price, status);
+    setFilter(true);
+  };
+  const handleSelectType = (e) => {
+    setType(e.target.value);
+    setFilter(true);
+  };
+  const handleSelectPrice = (e) => {
+    setPrice(e.target.value);
+    setFilter(true);
+  };
+  const handleSelectStatus = (e) => {
+    setStatus(e.target.value);
+    setFilter(true);
+  };
+
+  const handleReset = () => {
+    handleGetCar(name, type, price, status);
+    setFilter(false);
+  };
+
+  return (
+    <div>
+      <div className="search-dropdown">
+        <div>
+          <p>Nama Mobil</p>
+          <input onChange={handleChangeName} placeholder="cari mobil" />
+        </div>
+        <div>
+          <p>Kategori</p>
+          <select onChange={handleSelectType}>
+            <option value={""}>Masukan Kapasitas Mobil</option>
+            <option value={"medium"}>Medium</option>
+            <option value={"large"}>large</option>
+          </select>
+        </div>
+        <div>
+          <p>Harga</p>
+          <select onChange={handleSelectPrice}>
+            <option value={""}>Masukan Harga Sewa Perhari</option>
+            <option value={"50000"}>{"<700.000"}</option>
+            <option value={"350000"}>{"<350.000"}</option>
+            <option value={"330000"}>{"<330.000"}</option>
+          </select>
+        </div>
+        <div>
+          <p>Status</p>
+          <select value={status} onChange={handleSelectStatus}>
+            <option value={""}>Status</option>
+            <option value={"false"}>Avalaible</option>
+            <option value={"350000"}>NotAvalaible</option>
+          </select>
+        </div>
+        <div>
+          <button
+            onClick={isFilter ? handleReset : handleSubmit}
+            id="button-result"
+          >
+            {isFilter ? "reset" : "submit"}
+          </button>
+        </div>
+      </div>
+
+      <div className="car-list">
+        {car.map((item) => (
+          <Container>
+            <Row>
+              <Col>
+                <Card>
+                  <Card.Body>
+                    <Card.img src={item.image} />
+                    <Card.Title>
+                      <h4>{item.name}</h4>
+                      <h5>{item.price}</h5>
+                      <h6>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Explicabo atque velit tempora natus modi delectus
+                        amet labore voluptatum doloremque adipisci ullam, a
+                        itaque repudiandae hic.
+                      </h6>
+                    </Card.Title>
+                    <Link to={`/detail/${item.id}`}>
+                      <button variant="success"> Pilih Mobil</button>
+                    </Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CarResult;
---
+++ src/pages/Result/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+
+const Result = () => {
+  return <div>Result</div>;
+};
+
+export default Result;
